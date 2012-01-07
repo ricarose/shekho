c37e1fb61266f92366d3d75045c4a3a7abbdd188
Sayamindu Dasgupta <sayamindu@gmail.com>
2012-01-07 01:28:55 -0500

    Fix more indentation

--- session/forms.py
+++ session/forms.py
@@ -10,20 +10,20 @@ class SessionForm(ModelForm):
 
 class RegistrationForm(UserCreationForm):
 	username	= forms.CharField(max_length=30)
-    first_name = forms.CharField(max_length=30)
-    last_name = forms.CharField(max_length=30)
-    email = forms.EmailField(max_length=75)
-
-    class Meta:
-        model = User
-        fields = ("username", "first_name", "last_name", "email",)
-
-    def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.username = self.cleaned_data["username"]
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
+	first_name = forms.CharField(max_length=30)
+	last_name = forms.CharField(max_length=30)
+	email = forms.EmailField(max_length=75)
+	
+	class Meta:
+		model = User
+		fields = ("username", "first_name", "last_name", "email",)
+	
+	def save(self, commit=True):
+		user = super(RegistrationForm, self).save(commit=False)
+		user.username = self.cleaned_data["username"]
+		user.first_name = self.cleaned_data["first_name"]
+		user.last_name = self.cleaned_data["last_name"]
+		user.email = self.cleaned_data["email"]
+		if commit:
+			user.save()
+		return user
