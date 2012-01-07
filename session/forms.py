d0494b2e3c50e51a3fd9062cd2cbcbbbd9fb5351
Sayamindu Dasgupta <sayamindu@gmail.com>
2012-01-07 01:32:18 -0500

    Missing import (again)

--- session/forms.py
+++ session/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 import shekho.session.models
 
