a1dd3af40c0ecdb7864ec8290be6f5f9ea61d30d
Sayamindu Dasgupta <sayamindu@gmail.com>
2012-01-07 01:34:04 -0500

    Bugfix

--- session/views.py
+++ session/views.py
@@ -58,9 +58,9 @@ def register(request):
 
 	t = loader.get_template('registration/register.html')
     
-	if request.method == 'POST':
+    if request.method == 'POST':
 		form = RegistrationForm(request.POST)
-        if form.is_valid():
+		if form.is_valid():
 			new_user = form.save()
 			new_user = \
 				authenticate(username=request.POST['username'], \
