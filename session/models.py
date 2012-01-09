759c7c7f9e2415c9a534ae08eb3398cfe6c3b7fb
Sayamindu Dasgupta <sayamindu@gmail.com>
2012-01-09 12:53:45 -0500

    Increase size of type field

--- session/models.py
+++ session/models.py
@@ -14,11 +14,11 @@ class Session(models.Model):
         default='unconfirmed')
     #location = models.TextField(blank=True)
     #time = models.DateTimeField(blank=True)
-    type = models.CharField(max_length=30)
+    type = models.CharField(max_length=100)
     duration = models.CharField(max_length = 50)
     attendees = models.ManyToManyField(User, null=True, blank=True,\
         related_name="attendees")
-        
+
     def __unicode__(self):
     	return self.title
 
